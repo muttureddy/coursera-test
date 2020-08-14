e116fb7602b780f2f41ffde7bbcbe9b6a7f0ff48
muttureddy <muttureddy@users.noreply.github.com>
2020-08-14 19:03:02 +0530

    module 4 solution updated

--- module4_solution/SpeakHello.js
+++ module4_solution/SpeakHello.js
@@ -33,13 +33,17 @@ function speak(name) {
 // See Lecture 52, part 2
 
 
+// STEP 3: Create an object, called 'helloSpeaker' to which you will attach
+// the "speak" method and which you will expose to the global context
+// See Lecture 52, part 1
+ 
+
 (function () {
 	var helloSpeaker = {};
 	var speakWord = "Hello";
 
 	helloSpeaker.speak = function (name) {
   		console.log(speakWord + " " + name);
-  		
 	}
  	window.helloSpeaker = helloSpeaker;
 })();
